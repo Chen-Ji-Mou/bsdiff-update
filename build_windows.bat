32756b826f60ff81ab9fedd0b915853bf8202a91
陈垍谋 <1721554825@qq.com>
2022-09-01 00:14:37 +0800

    [Commit] 2022.09.01 00:14

--- build_windows.bat
+++ build_windows.bat
@@ -4,10 +4,10 @@ rmdir /s /q %ROOT%\outputs
 mkdir %ROOT%\outputs
 
 cd %ROOT%\original_app
-%ROOT%\original_app\gradlew.bat --parallel assembleRelease
+call %ROOT%\original_app\gradlew.bat --parallel assembleRelease
 
 cd %ROOT%\new_app
-%ROOT%\new_app\gradlew.bat --parallel assembleRelease
+call %ROOT%\new_app\gradlew.bat --parallel assembleRelease
 
 cd %ROOT%\bsdiff
 bsdiff %ROOT%\outputs\original-app-release.apk %ROOT%\outputs\new-app-release.apk %ROOT%\outputs\patch
